c5456ed54ff475e8c57911b9bb27b143e9289846
christiansegura <segurac@pdx.odshp.com>
2024-05-16 07:35:42 -0700

    added uikit css framework added nav added form and classes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {StockInventoryModule} from './stock-inventory/stock-inventory.module';
+import {NavComponent} from './core/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StockInventoryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/stock-inventory/containers/stock-inventory.component.ts
@@ -0,0 +1,60 @@
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, FormArray} from '@angular/forms';
+
+@Component({
+  selector: 'stock-inventory',
+  styleUrls: ['stock-inventory.component.scss'],
+  template: `
+    <h1 class="uk-margin-top">Computer Inventory</h1>
+    <form class="uk-form-stacked" [formGroup]="form" (ngSubmit)="onSubmit()">
+
+      <div formGroupName="store">
+        <div class="uk-margin">
+          <label class="uk-form-label" for="form-stacked-text">Branch id</label>
+          <div class="uk-form-controls">
+            <input formControlName="branch" class="uk-input" id="form-stacked-text" type="text" placeholder="Branch id">
+          </div>
+        </div>
+        <div class="uk-margin">
+          <label class="uk-form-label" for="form-stacked-text">Manager Code</label>
+          <div class="uk-form-controls">
+            <input formControlName="code" class="uk-input" id="form-stacked-text" type="text" placeholder="Manager Code">
+          </div>
+        </div>
+      </div>
+
+      <div class="uk-margin">
+        <label class="uk-form-label" for="form-stacked-select">Select</label>
+        <div class="uk-form-controls">
+          <select class="uk-select" id="form-stacked-select">
+            <option>Option 01</option>
+            <option>Option 02</option>
+          </select>
+        </div>
+      </div>
+
+      <div class="uk-margin">
+        <div class="uk-form-label">Radio</div>
+        <div class="uk-form-controls">
+          <label><input class="uk-radio" type="radio" name="radio1"> Option 01</label><br>
+          <label><input class="uk-radio" type="radio" name="radio1"> Option 02</label>
+        </div>
+      </div>
+      <button class="uk-button uk-button-primary" type="submit" [disabled]="form.invalid">Order stock</button>
+      <pre>{{form.value | json}}</pre>
+    </form>
+  `
+})
+
+export class StockInventoryComponent {
+  form = new FormGroup({
+    store: new FormGroup({
+      branch: new FormControl('321'),
+      code: new FormControl('fds')
+    })
+  })
+
+  onSubmit() {
+    console.log('Submit:', this.form.value)
+  }
+}
